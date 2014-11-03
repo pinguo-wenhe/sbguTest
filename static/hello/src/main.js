2bf6e11a71ca56068ce9596b1320e0584a3f61f2
pinguo-wenhe <wenhe@camera360.com>
2014-11-03 17:33:58 +0800

    first_comm for sbuhttps://github.com/pinguo-wenhe/sbguTest.git

---
+++ static/hello/src/main.js
@@ -0,0 +1,17 @@
+define(function(require,exports,module){
+
+	var $ = require('jquery');
+
+	var Spinning = require('./spinning').spinning;
+	
+	var p = $('p');
+	console.log($);
+	console.log('s');
+	console.log(p);
+	console.log('s');
+	// exports.sayHello = function(){
+	// 	alert("hello world");
+	// }
+
+	console.log(Spinning);
+})
